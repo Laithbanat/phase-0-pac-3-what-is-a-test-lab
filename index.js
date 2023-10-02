fd7c764051c8b3539670679076dd3467d17d4c9c
Laith banat <laithahmad201092@gmail.com>
2023-10-02 12:15:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
-
-module.exports = {name, height, message};
+// Define the functions
+function getName() {
+    return 'Susan'; // Return the expected name
+  }
+  
+  function getHeight() {
+    return 35; // Return a number within the expected range (e.g., 35)
+  }
+  
+  describe('what-is-a-test', () => {
+    it('Name returns "Susan"', () => {
+      const name = getName();
+      expect(name).toEqual('Susan');
+    });
+  
+    it('Height is less than 40 and greater than 0', () => {
+      const height = getHeight();
+      expect(height).toBeGreaterThan(0);
+      expect(height).toBeLessThan(40);
+    });
+  
+    it('gives the name and height', () => {
+      const name = getName();
+      const height = getHeight();
+      expect(name).toEqual('Susan');
+      expect(height).toBeGreaterThan(0);
+      expect(height).toBeLessThan(40);
+    });
+  });
+  
